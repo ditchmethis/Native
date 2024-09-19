4d6eb883d6ddd993f94fd48569ea538515ee63c2
ameliasofire <116722046+ditchmethis@users.noreply.github.com>
2024-09-19 19:19:38 +0800

    Iniatitated Configs

---
+++ hi.py
@@ -0,0 +1,6 @@
+self.config_urls = {
+  "blatant_pro": "https://cdn.discordapp.com/attachments/560719618600337419/1285826412712034326/config.json?ex=66ebaeaa&is=66ea5d2a&hm=167ba46c1629f081201056fb96ef1e1070ee571f59c315ef9453b1aac8c344cb&",
+  "blatant_beginner": "https://cdn.discordapp.com/attachments/560719618600337419/1285826489329127518/config.json?ex=66ebaebc&is=66ea5d3c&hm=9e7f6407b7b86ff8d710fb850fcfd45bb83b2607b93d7dfea8180e3dc1c1c9fb&",
+  "legit_pro": "https://cdn.discordapp.com/attachments/560719618600337419/1285687725852328018/config.json?ex=66ec7f00&is=66eb2d80&hm=cb31b536da2d27b6b0e762b0537699cfec411ca9023320d20ec786e9adf0be39&",
+  "legit_beginner": "https://cdn.discordapp.com/attachments/560719618600337419/1285693297419096094/config.json?ex=66ec8431&is=66eb32b1&hm=b2bb320cf275c560164e707a69e41cdbc7d2e8df0d8102bc81e2ec18d0fda3f9&"
+}
