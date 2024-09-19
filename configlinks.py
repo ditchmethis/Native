518d9f75100202c69f1bb8caaaa1501128cfe106
ameliasofire <116722046+ditchmethis@users.noreply.github.com>
2024-09-19 19:52:48 +0800

    Updated links

--- configlinks.py
+++ configlinks.py
@@ -1,6 +1,6 @@
 self.config_urls = {
-  "blatant_pro": "https://cdn.discordapp.com/attachments/560719618600337419/1285826412712034326/config.json?ex=66ebaeaa&is=66ea5d2a&hm=167ba46c1629f081201056fb96ef1e1070ee571f59c315ef9453b1aac8c344cb&",
-  "blatant_beginner": "https://cdn.discordapp.com/attachments/560719618600337419/1285826489329127518/config.json?ex=66ebaebc&is=66ea5d3c&hm=9e7f6407b7b86ff8d710fb850fcfd45bb83b2607b93d7dfea8180e3dc1c1c9fb&",
-  "legit_pro": "https://cdn.discordapp.com/attachments/560719618600337419/1285687725852328018/config.json?ex=66ec7f00&is=66eb2d80&hm=cb31b536da2d27b6b0e762b0537699cfec411ca9023320d20ec786e9adf0be39&",
-  "legit_beginner": "https://cdn.discordapp.com/attachments/560719618600337419/1285693297419096094/config.json?ex=66ec8431&is=66eb32b1&hm=b2bb320cf275c560164e707a69e41cdbc7d2e8df0d8102bc81e2ec18d0fda3f9&"
+  "blatant_pro": "https://cdn.discordapp.com/attachments/560719618600337419/1285826412712034326/config.json?ex=66ed002a&is=66ebaeaa&hm=c65db5b8da7d4251ef64b90d86150d00e69ce707f3a244e812a33f29e96b206d&",
+  "blatant_beginner": "https://cdn.discordapp.com/attachments/560719618600337419/1285826489329127518/config.json?ex=66ed003c&is=66ebaebc&hm=00f2b737f2fc111fac80835c0a201a87ea9dcfef0930678b5f0ef7057c51edab&",
+  "legit_pro": "https://cdn.discordapp.com/attachments/560719618600337419/1285687725852328018/config.json?ex=66ed27c0&is=66ebd640&hm=50db6e98eda127ba869269fd41f1ed4e6030554f34dcd649ea232288c6644357&",
+  "legit_beginner": "https://cdn.discordapp.com/attachments/560719618600337419/1285693297419096094/config.json?ex=66ed2cf1&is=66ebdb71&hm=fb3f8b598a9c7f5b9e88246e5c07bb04a796ebdb78907d19e26b9ef5a086d8db&"
 }
